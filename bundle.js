41178b7a63fd5b552ddf1f1564c38662c23c3b1e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-08-30 10:57:46 +0000

    Deploy lokothodida/uk-politics-db to github.com/lokothodida/uk-politics-db.git:gh-pages

--- bundle.js
+++ bundle.js
@@ -10287,18 +10287,53 @@ if (inBrowser1 && window.Vue) {
     window.Vue.use(VueRouter);
 }
 Vue.use(VueRouter);
-const HomePage = {
-    template: `<div>
+const SQL = await initSqlJs({
+    locateFile: (file)=>`https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.6.1/${file}`
+});
+const db = new SQL.Database(new Uint8Array([]));
+const HomePage = (db1)=>({
+        data () {
+            return {
+                query: "",
+                results: []
+            };
+        },
+        template: `<div>
         <h1>UK Politics DB</h1>
-        <p>WIP...</p>
-    </div>`
-};
+        <p>Write your SQL queries into the below space and hit <code>Execute</code> to see results.</p>
+        <div><textarea v-model="query"></textarea></div>
+        <button v-on:click.prevent="execute">Execute</button>
+
+        <table v-for="result in results">
+            <thead>
+                <tr>
+                    <th v-for="column in result.columns">{{column}}</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="row in result.values">
+                    <td v-for="field in row">{{field}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>`,
+        methods: {
+            execute () {
+                try {
+                    this.results = db1.exec(this.query);
+                } catch (err) {
+                    alert(`Failed to execute query: ${err.message}`);
+                }
+            }
+        }
+    })
+;
 new Vue({
     router: new VueRouter({
         routes: [
             {
                 path: "/",
-                component: HomePage
+                component: HomePage(db)
             }, 
         ]
     })
