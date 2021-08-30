2621aaebaf7d52835bc8b9f481ebffa3691398b0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-08-30 11:32:19 +0000

    Deploy lokothodida/uk-politics-db to github.com/lokothodida/uk-politics-db.git:gh-pages

--- bundle.js
+++ bundle.js
@@ -10286,6 +10286,39 @@ VueRouter.START_LOCATION = START;
 if (inBrowser1 && window.Vue) {
     window.Vue.use(VueRouter);
 }
+const loadTables = (tables, db)=>{
+    return Promise.all(tables.map(async (table)=>{
+        const resp = await fetch(`/tables/${table}.sql`);
+        if (resp.status >= 400) {
+            throw fileNotFound(`${table}.sql`);
+        }
+        const script = await resp.text();
+        db.exec(script);
+    }));
+};
+const load2015Election = async (db)=>{
+    const { data: data2  } = await loadCsv("ge-2015");
+    data2.map((item)=>{
+        db.exec(`INSERT OR IGNORE INTO constituencies(ons_id, ons_region_id, constituency_name) VALUES(?, ?, ?)`, [
+            item.ons_id,
+            item.ons_region_id,
+            item.constituency_name
+        ]);
+    });
+};
+const loadCsv = async (name)=>{
+    const resp = await fetch(`/data/${name}.csv`);
+    if (resp.status >= 400) {
+        throw fileNotFound(`${name}.csv`);
+    }
+    const blob = await resp.text();
+    return Papa.parse(blob, {
+        header: true,
+        skipEmptyLines: true
+    });
+};
+const fileNotFound = (file)=>new Error(`File ${file} not found`)
+;
 Vue.use(VueRouter);
 const SQL = await initSqlJs({
     locateFile: (file)=>`https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.6.1/${file}`
@@ -10294,29 +10327,54 @@ const db = new SQL.Database(new Uint8Array([]));
 const HomePage = (db1)=>({
         data () {
             return {
+                loading: true,
+                error: false,
+                errorMessage: "",
                 query: "",
                 results: []
             };
         },
         template: `<div>
         <h1>UK Politics DB</h1>
-        <p>Write your SQL queries into the below space and hit <code>Execute</code> to see results.</p>
-        <div><textarea v-model="query"></textarea></div>
-        <button v-on:click.prevent="execute">Execute</button>
+        <div v-if="loading">
+            Loading...
+        </div>
+        <div v-if="error">
+            <code>{{errorMessage}}</code>
+        </div>
+        <div v-else>
+            <p>Write your SQL queries into the below space and hit <code>Execute</code> to see results.</p>
+            <div><textarea v-model="query"></textarea></div>
+            <button v-on:click.prevent="execute">Execute</button>
 
-        <table v-for="result in results">
-            <thead>
-                <tr>
-                    <th v-for="column in result.columns">{{column}}</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="row in result.values">
-                    <td v-for="field in row">{{field}}</td>
-                </tr>
-            </tbody>
-        </table>
+            <table v-for="result in results">
+                <thead>
+                    <tr>
+                        <th v-for="column in result.columns">{{column}}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="row in result.values">
+                        <td v-for="field in row">{{field}}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>`,
+        async mounted () {
+            try {
+                await loadTables([
+                    "constituencies"
+                ], db1);
+                await load2015Election(db1);
+            } catch (err) {
+                this.error = true;
+                this.errorMessage = err.message;
+                console.error(err);
+            } finally{
+                this.loading = false;
+            }
+        },
         methods: {
             execute () {
                 try {
